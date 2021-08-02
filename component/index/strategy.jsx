5af82b8148a4e486288760b85bf99988b8b449e7
SanB <sandro.brignoli@outlook.fr>
2021-08-02 13:48:05 +0200

    🎨 Update Strategy

--- component/index/strategy.jsx
+++ component/index/strategy.jsx
@@ -2,6 +2,9 @@ const Strategy = () => {
   return (
     <div className='strategy'>
       <div className='strategy__content'>
+        <div className='strategy__content-img'>
+          <img src='/images/plan.svg' />
+        </div>
         <div className='strategy__content-yellow-form'></div>
       </div>
     </div>
