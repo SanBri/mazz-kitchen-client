dec48f96ad758580eb7b1b680c0e15a56a06279d
SanB <sandro.brignoli@outlook.fr>
2021-08-12 13:44:16 +0200

    📸 Add Images

--- component/staff/why.jsx
+++ component/staff/why.jsx
@@ -1,3 +1,5 @@
+import Icon from "../common/icon";
+
 const Why = () => {
   return (
     <div className='why-container'>
@@ -7,43 +9,73 @@ const Why = () => {
         </div>
         <div className='why__content'>
           <div className='why__content-left'>
+            <div className='why__content-left__image only-mobile'>
+              <Icon src='/images/staff/image01.svg' width='auto' height={300} />
+            </div>
             <p>
               Rien de tel qu’une belle bio pour faire connaissance, découvrez
-              notre parcours, nos objectifs, nos compétences au niveau du design
-              et développement web, ainsi que des informations essentielles sur
-              nos tarifs.
+              <span className='important'>
+                {" "}
+                notre parcours, nos objectifs, nos compétences
+              </span>{" "}
+              au niveau du{" "}
+              <span className='important'>design et développement web</span>,
+              ainsi que des informations essentielles sur nos tarifs.
             </p>
             <p>
-              Après plusieurs années dans la restauration nous avons décidés de
-              créer cette agence de communication spécialisée dans les métiers
-              de bouche par passion pour le métier de restaurateur/commerçant et
-              par volonté de donner un nouveau souffle à ce marché.
+              Après plusieurs années dans{" "}
+              <span className='important'> la restauration</span> nous avons
+              décidés de créer cette{" "}
+              <span className='important'>
+                {" "}
+                agence de communication spécialisée dans les métiers de bouche
+              </span>{" "}
+              par passion pour le métier de restaurateur/commerçant et par
+              volonté de donner{" "}
+              <span className='important'>
+                {" "}
+                un nouveau souffle à ce marché.
+              </span>
             </p>
             <p>
-              Ensemble nous construisons pas à pas votre identité digitale et
-              analysons les besoins et les possibilités qui s’offrent à vous
-              afin de dynamiser votre image et donner la visibilité nécessaire à
-              votre projet !
+              Ensemble nous construirons pas à pas votre{" "}
+              <span className='important'> identité digitale</span> et analysons
+              les <span className='important'>besoins</span> et les{" "}
+              <span className='important'> possibilités</span> qui s’offrent à
+              vous afin de <span className='important'> dynamiser</span> votre
+              image et donner la <span className='important'> visibilité</span>{" "}
+              nécessaire à votre projet !
             </p>
           </div>
           <div className='why__content-right'>
+            <div className='why__content-right__image only-mobile'>
+              <Icon src='/images/staff/image02.svg' width='auto' height={300} />
+            </div>
             <p>
-              Mazz Kitchen, c’est la recette idéale adaptée à la communication
-              digitale des métiers de bouches, des commerces de proximité, des
-              entrepreneurs et des artisans.
+              Mazz Kitchen, c’est{" "}
+              <span className='important'>la recette idéale</span> adaptée à la
+              communication digitale des métiers de bouches, des commerces de
+              proximité, des entrepreneurs et des artisans.
             </p>
             <p>
-              Réaliser une identité visuelle, c’est comme suivre une bonne
-              recette de cuisine, si les proportions et les indications sont
-              respectées alors vous aurez un bon plat. Si votre créativité et
-              votre curiosité s’en mêlent, vous aurez un plat exceptionnel.
-              Ensemble nous trouvons la recette idéale à votre projet.
+              Réaliser une identité visuelle, c’est comme suivre{" "}
+              <span className='important'>une bonne recette de cuisine</span>,
+              si les proportions et les indications sont respectées alors vous
+              aurez un bon plat. Si votre créativité et votre curiosité s’en
+              mêlent, vous aurez{" "}
+              <span className='important'>un plat exceptionnel</span>. Ensemble,
+              <span className='important'>
+                trouvons la recette idéale à votre projet.
+              </span>
             </p>
             <p>
-              Au menu : une stratégie de communication digitale clé en main afin
-              de développer votre image sur le web et vous ouvrir de nouvelles
-              possibilités. Notre équipe est là pour vous faciliter la tâche et
-              vous accompagner afin de mettre en place votre projet !
+              Au menu :{" "}
+              <span className='important'>
+                une stratégie de communication digitale clé en main
+              </span>{" "}
+              afin de développer votre image sur le web et vous ouvrir de
+              nouvelles possibilités. Notre équipe est là pour vous faciliter la
+              tâche et vous accompagner afin de mettre en place votre projet !
             </p>
           </div>
         </div>
--- component/tarifs/offers.jsx
+++ component/tarifs/offers.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import Icon from "../common/icon";
 
 const Offers = () => {
   return (
@@ -8,30 +8,50 @@ const Offers = () => {
       </div>
       <div className='offers__content'>
         <div className='offers__content-left'>
-          <p>Nous vous proposons une stratégie de communication clé en main.</p>
+          <div className='offers__content-left__image'>
+            <Icon src='/images/tarifs/image01.svg' width='auto' height={300} />
+          </div>
+          <p>
+            Nous vous proposons{" "}
+            <span className='important'>
+              une stratégie de communication clé en main.
+            </span>
+          </p>
           <p>
             Afin de permettre à chacun d’avoir une transparence quant aux tarifs
-            réalisés par le Studio, nous avons choisi de déterminer un prix de
-            base de 30€/heure* sachant que chaque projet est évalué selon la
-            contrainte horaire et l’exigence de réalisation de celui-ci.
+            réalisés par le Studio, nous avons choisi de déterminer un{" "}
+            <span className='important'>prix de base de 40€/heure</span>*,
+            chaque projet étant évalué selon la{" "}
+            <span className='important'>contrainte horaire</span> et{" "}
+            <span className='important'>l’exigence de réalisation</span> de
+            celui-ci.
           </p>
           <p>
-            Nous partons du principe que chaque projet est unique et nécessite
-            un plan de travail et une mise en place particulière. L’idée est
-            d’offrir les conditions optimales à votre projet et à notre
-            activité.​
+            Nous partons du principe que{" "}
+            <span className='important'>chaque projet est unique</span> et
+            nécessite un plan de travail et une mise en place particulière.
+            L’idée est d’offrir les{" "}
+            <span className='important'>conditions optimales</span> à votre
+            projet et à notre activité.​
           </p>
           <p>
-            Nous choisirons, ensemble, la meilleure recette pour votre projet,
-            ce qui nous permettra de mettre en place celui-ci dans des
+            Nous choisirons ensemble{" "}
+            <span className='important'>
+              la meilleure recette pour votre projet
+            </span>
+            , ce qui nous permettra de mettre en place celui-ci dans des
             conditions optimales et les meilleurs délais.
           </p>
           <p>
-            Les prix indiqués dans le devis ne tiennent compte que du service
-            réalisé par le Studio, sachant que les prestations extérieures ne
-            peuvent être facturées par le Studio. Cependant des prestataires
-            vous seront proposés avec des tarifs préférentiels sans frais de
-            commission de la part du Studio.
+            Les prix indiqués dans le devis ne tiennent compte que du{" "}
+            <span className='important'>service réalisé par le Studio</span>,
+            sachant que les prestations extérieures ne peuvent être facturées
+            par le Studio. Cependant,{" "}
+            <span className='important'>
+              des prestataires vous seront proposés
+            </span>{" "}
+            avec des tarifs préférentiels sans frais de commission de la part du
+            Studio.
           </p>
           <div className='condition-text'>
             <p>
@@ -41,33 +61,53 @@ const Offers = () => {
           </div>
         </div>
         <div className='offers__content-right'>
-          <p>​Vous êtes un acteur de la restauration ? Un commerçant ?</p>
+          <div className='offers__content-right__image'>
+            <Icon src='/images/tarifs/image02.svg' width='auto' height={300} />
+          </div>
           <p>
-            Un artisan ? Un entrepreneur ? Nous sommes faits pour travailler
-            ensemble !​
+            ​Vous êtes un acteur de la{" "}
+            <span className='important'>restauration</span> ? Un{" "}
+            <span className='important'>commerçant</span> ? Un{" "}
+            <span className='important'>artisan</span> ? Un{" "}
+            <span className='important'>entrepreneur</span> ? Nous sommes faits
+            pour <span className='important'>travailler ensemble</span> !​
           </p>
           <p>
-            Quel que soit votre projet web nous saurons vous accompagner afin de
-            donner le ton et l’harmonie à votre image et développer avec vous
-            une communication interactive et dynamique.
+            Quel que soit <span className='important'>votre projet web</span>{" "}
+            nous saurons vous accompagner afin de donner{" "}
+            <span className='important'>le ton et l’harmonie</span> à votre
+            image et développer avec vous{" "}
+            <span className='important'>
+              une communication interactive et dynamique.
+            </span>
           </p>
           <p>
-            ​Si vous ne savez pas trop où aller et comment mettre en place votre
-            communication, il vous suffit de cliquer ici ou contactez nous
-            directement par mail ou par téléphone !
+            ​Si vous ne savez pas trop{" "}
+            <span className='important'>où aller</span> et{" "}
+            <span className='important'>
+              comment mettre en place votre communication
+            </span>
+            , il vous suffit de <span className='link'>cliquer ici</span> ou
+            contactez nous directement par mail ou par téléphone !
           </p>
           <p>
             Et si vous voulez en savoir plus sur notre travail, il vous suffit
-            de consulter nos profils.
+            de <span className='link'>consulter nos profils</span>.
           </p>
           <p>
-            ​Nous vous proposons la mise en place d’une stratégie de
-            communication digitale complète, interactive et innovante, mais vous
-            pouvez également bénéficier d’un service en particulier selon vos
-            besoins, nous vous mettrons en contact avec les membres de l’équipe
+            ​Nous vous proposons la mise en place d’une{" "}
+            <span className='important'>
+              stratégie de communication digitale complète
+            </span>
+            , interactive et innovante, mais vous pouvez également bénéficier
+            d’un{" "}
+            <span className='important'>
+              service en particulier selon vos besoins
+            </span>
+            , nous vous mettrons en contact avec les membres de l’équipe
             concernés par votre demande.
           </p>
-          <p>On vous attend !​</p>
+          <p className='important'>On vous attend !​</p>
         </div>
       </div>
     </div>
